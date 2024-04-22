7ed2a54ecfd9b7d92d290b1777ca49053bc29ee3
zwelisha <zwescientist@gmail.com>
2024-04-22 19:12:24 +0200

    Added documentation and gitignroe file

---
+++ README.md
@@ -0,0 +1,82 @@
+# COVI-19 Data Visualisation
+
+## Description
+- Visualise the supplied data in a graph of some sort programmatically.
+- You may use any tools or languages that you would like (please do not use Excel).
+- When submitting your code, please provide a detailed explanation of how your code works.
+- Also, please detail any external libraries that may be required for your code to work
+
+## Getting Started
+
+### Requirements
+
+1. Python 3
+2. Pip 
+3. Text editor (Recommended: VSCode or SublimeText or Atom)
+4. Dash
+5. Black
+   
+### Installation
+
+#### 1. Python 3
+
+The installation differs from one operating system to the other, but the documentation which can be found [here](https://www.python.org/downloads/) have clear instructions for each operating system.
+
+#### 2. Pip
+
+pip is the package installer for Python. 
+Details [here](https://pypi.org/project/pip/)
+
+#### 3. Text Editor
+Any text editor of your choice can be used. However Visual Studio Code and Atom are highly recommended. See [VS Code](https://code.visualstudio.com/) and [Atom](https://atom-editor.cc/)
+
+#### 4. Dash
+Run ```pip install dash```
+
+#### 5. Black
+I use Black to format the code to conform to PEP 8 standard.
+```pip install black```
+
+#### Folder Structure
+
+There is one folder in this project with every file needed to run the code.
+
+```.gitignore
+    app.py
+    covid.json
+    README.md
+```
+
+### Formatting The Code
+
+Use Black for consistency
+Run ```black app.py``` to format the code after making changes.
+
+### Running The Project
+Run ```python app.y```
+Then click on the link to see the output or copy and paste the link on your browser.
+
+#### 1. Clone the project
+```
+git clone https://github.com/zwelisha/covid-19-data-viz.git
+```
+
+#### 2. Change Directory (To the root folder of the project)
+
+Change directory to the project `cd covid-19-data-viz`.
+There you will see the folder structure detailed above.
+
+Then to run this app run the following command
+
+```
+python app.py
+```
+
+### Testing the code
+There are no tests yet, but I prefer pytest.
+
+Have fun
+
+#### Author
+
+[Zweli Mthethwa](https://www.linkedin.com/in/zweli-mthethwa-244b45a8/)
--- app.py
+++ app.py
@@ -5,54 +5,68 @@ from dash.dependencies import Input, Output
 import plotly.graph_objs as graph_obj
 import pandas as pd
 
-file = open('covid.json', 'r')
+
+file = open("covid.json", "r")
 data = json.load(file)
 file.close()
 
 df = pd.DataFrame(data)
 
-df['Date'] = pd.to_datetime(df['Date'])
+df["Date"] = pd.to_datetime(df["Date"])
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
-    children = [
-        html.H1("COVID-19 Data Analysis and Visualisation", style={'color': 'white', 'backgroundColor': 'maroon', 'padding': '15px'}),
+    children=[
+        html.Title("Zweli Visuals"),
+        html.H1(
+            "COVID-19 Data Analysis and Visualisation",
+            style={"color": "white", "backgroundColor": "maroon", "padding": "15px"},
+        ),
         html.Label("Select variable to plot and visualise:"),
         dcc.Dropdown(
-            id='variable-label',
+            id="variable-label",
             options=[
-                {'label': 'Total Confirmed Cases', 'value': 'Total Confirmed Cases'},
-                {'label': 'Total Deaths', 'value': 'Total Deaths'},
-                {'label': 'Total Recovered', 'value': 'Total Recovered'},
-                {'label': 'Active Cases', 'value': 'Active Cases'},
-                {'label': 'Daily Confirmed Cases', 'value': 'Daily Confirmed Cases'},
-                {'label': 'Daily Deaths', 'value': 'Daily Deaths'}
+                {"label": "Total Confirmed Cases", "value": "Total Confirmed Cases"},
+                {"label": "Total Deaths", "value": "Total Deaths"},
+                {"label": "Total Recovered", "value": "Total Recovered"},
+                {"label": "Active Cases", "value": "Active Cases"},
+                {"label": "Daily Confirmed Cases", "value": "Daily Confirmed Cases"},
+                {"label": "Daily Deaths", "value": "Daily Deaths"},
             ],
-            value='Total Confirmed Cases'
+            value="Total Confirmed Cases",
         ),
-        dcc.Graph(id='covid-line-graph')
+        dcc.Graph(id="covid-line-graph"),
+        html.Footer(
+            children=[
+                html.Div(
+                    style={
+                        "color": "maroon",
+                    },
+                    children=[html.P("copyright @2024 Developed By Zweli Mthethwa")],
+                )
+            ]
+        ),
     ]
 )
 
-@app.callback(
-    Output('covid-line-graph', 'figure'),
-    [Input('variable-label', 'value')]
-)
+
+@app.callback(Output("covid-line-graph", "figure"), [Input("variable-label", "value")])
 def update_graph(selected_data):
     trace = graph_obj.Scatter(
-        x=df['Date'],
+        x=df["Date"],
         y=df[selected_data],
-        mode='lines+markers',
-        marker=dict(color='maroon'),
-        name=selected_data
+        mode="lines+markers",
+        marker=dict(color="maroon"),
+        name=selected_data,
     )
     layout = graph_obj.Layout(
-        title='COVID-19 Data Visualisation for the variable selected above',
-        xaxis=dict(title='Date'),
-        yaxis=dict(title=selected_data)
+        title="COVID-19 insights from March 2020 to June 2020 for the selected variable",
+        xaxis=dict(title="Date"),
+        yaxis=dict(title=selected_data),
     )
-    return {'data': [trace], 'layout': layout}
+    return {"data": [trace], "layout": layout}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
